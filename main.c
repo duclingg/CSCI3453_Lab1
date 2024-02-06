19d62502f17941432ef917bad987e2a80dece612
Justin Hoang <justin.lhoang3@gmail.com>
2024-02-05 23:54:01 -0700

    matrix numbers are not statically defined anymore

--- main.c
+++ main.c
@@ -1,89 +1,107 @@
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<stdlib.h>
 
-#define M 4
-#define K 3
-#define N 4
-#define THREADS 5
+#define MAX 4
 
-int A[M][K] = {{2,1,3}, {0,-1,-2}, {5,1,-1}, {4,5,8}};
-int B[K][N] = {{3,2,1,0}, {0,4,2,-1}, {-1,3,2,4}};
-int C[M][N];
-
-struct v {
-    int i;
-    int j;
-};
-
-void *runner(void *param);
-
-int main(int argc, char *argv[]) {
-    int i, j, count = 0;
-
-    // print Matrix A
-    printf("Matrix A:\n");
-    for(i = 0; i < M; i++) {
-        for(j = 0; j < K; j++) {
-            printf("%d ", A[i][j]);
-        }
-
-        printf("\n");
+// each thread computes single element in the resultant matrix
+void *mult(void* arg) {
+	int *data = (int *)arg;
+	int k = 0, i = 0;
+	
+	int x = data[0];
+	for (i = 1; i <= x; i++) {
+		k += data[i]*data[i+x];
     }
-
-    printf("\n");
-
-    // print Matrix B
-    printf("Matrix B:\n");
-    for(i = 0; i < K; i++) {
-        for(j = 0; j < N; j++) {
-            printf("%d ", B[i][j]);
-        }
-
-        printf("\n");
-    }
-
-    printf("\n");
-
-    // multiply Matrix A and B
-    for(i = 0; i < M; i++) {
-        for(j = 0; j < N; j++) {
-            struct v *data = (struct v*)malloc(sizeof(struct v));
-            data->i = i;
-            data->j = j;
-
-            pthread_t tid;
-            pthread_attr_t attr;
-
-            pthread_attr_init(&attr);
-
-            pthread_create(&tid, &attr, runner, data);
-
-            pthread_join(tid, NULL);
-            count++;
-        }
-    }
-
-    // print resulting Matrix C 
-    printf("Matrix C: A x B \n");
-    for(i = 0; i < M; i++) {
-        for(j = 0; j < N; j++) {
-            printf("%d ", C[i][j]);
-        }
-
-        printf("\n");
-    }
+	
+	int *p = (int*)malloc(sizeof(int));
+	*p = k;
+	
+    // terminates a thread and return value is passed as pointer
+	pthread_exit(p);
 }
 
-void *runner(void *param) {
-    struct v *data = param;
-    int n, sum = 0;
+int main() {
+	int matA[MAX][MAX]; 
+	int matB[MAX][MAX]; 
+	
+	int r1=MAX,c1=MAX,r2=MAX,c2=MAX,i,j,k;
 
-    for(n = 0; n < K; n++) {
-        sum += A[data->i][n] * B[n][data->j];
+	// generates random values in matrix A
+	for (i = 0; i < r1; i++) {
+		for (j = 0; j < c1; j++) {
+			matA[i][j] = rand() % 10; 
+        }
+    } 
+		
+	// generates random values in matrix B 
+	for (i = 0; i < r1; i++) {
+		for (j = 0; j < c1; j++) {
+			matB[i][j] = rand() % 10; 
+        }
+    }
+	
+	// displays matrix A	
+    printf("Matrix A:\n");	 
+	for(i = 0; i < r1; i++) {
+		for(j = 0; j < c1; j++) {
+			printf("%d ", matA[i][j]);
+        }
+		printf("\n");
+	}
+			
+	// displays matrix B
+    printf("Matrix B:\n");	 
+	for(i = 0; i < r2; i++) {
+		for(j = 0; j < c2; j++) {
+			printf("%d ", matB[i][j]);
+        }
+		printf("\n"); 
+	}
+	
+	int max = r1*c2;
+	
+	// declares array of threads of size r1*c2	 
+	pthread_t *threads;
+	threads = (pthread_t*)malloc(max*sizeof(pthread_t));
+	
+	int count = 0;
+	int* data = NULL;
+	for (i = 0; i < r1; i++) {
+		for (j = 0; j < c2; j++) {
+			// stores row and column elements in data 
+			data = (int *)malloc((20)*sizeof(int));
+			data[0] = c1;
+	
+			for (k = 0; k < c1; k++) {
+				data[k+1] = matA[i][k];
+            }
+	
+			for (k = 0; k < r2; k++) {
+				data[k+c1+1] = matB[k][j];
+            }
+			
+			// creates threads
+			pthread_create(&threads[count++], NULL, 
+			mult, (void*)(data));
+		}
+    }
+	
+	printf("Matrix C (A x B):\n");
+
+	for (i = 0; i < max; i++) {
+	    void *k;
+	
+	    // joins all threads and collecting return value 
+	    pthread_join(threads[i], &k);
+	    int *p = (int *)k;
+	    printf("%d ",*p);
+
+	    if ((i + 1) % c2 == 0) {
+		    printf("\n");
+        }
     }
 
-    C[data->i][data->j] = sum;
-
-    pthread_exit(0);
+	return 0;
 }
